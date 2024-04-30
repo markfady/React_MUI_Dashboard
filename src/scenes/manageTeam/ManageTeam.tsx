b9425a93c0e5d33c30ce2b3c3c4a4d7c79e863d5
Markfady <markfady9@gmail.com>
2024-04-30 12:49:10 +0300

    Header usage

---
+++ src/scenes/manageTeam/ManageTeam.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+
+const ManageTeam = () => {
+  return (
+    <Box m="20px">
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+    <Header title='MANAGE TEAM' subTitle='Welcome to your ManageTeam'/>
+    </Box>
+  </Box>
+  )
+}
+
+export default ManageTeam
